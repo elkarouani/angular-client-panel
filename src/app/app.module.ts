7c37d4586f4088cfd6ca8b067607c55374be796c
KDragon <elkarouani@gmail.com>
2020-01-29 16:45:41 +0100

    Task: adding services to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { AddClientComponent } from './components/add-client/add-client.component';
-import { EditClientComponent } from './components/edit-client/edit-client.component';
-import { DetailsClientComponent } from './components/details-client/details-client.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ClientsComponent } from "./components/clients/clients.component";
+import { AddClientComponent } from "./components/add-client/add-client.component";
+import { EditClientComponent } from "./components/edit-client/edit-client.component";
+import { DetailsClientComponent } from "./components/details-client/details-client.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { SettingsComponent } from "./components/settings/settings.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { ClientService } from "./services/client.service";
+import { AuthClientService } from "./services/auth-client.service";
 
 @NgModule({
   declarations: [
@@ -30,11 +32,8 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     RegisterComponent,
     NotFoundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [ClientService, AuthClientService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

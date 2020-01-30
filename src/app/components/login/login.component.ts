793895f99c393424b8688f38ed9757acc1f09895
KDragon <elkarouani@gmail.com>
2020-01-30 10:47:19 +0100

    Task: adding login method on auth client service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthClientService } from "src/app/services/auth-client.service";
+import { FlashMessagesService } from "angular2-flash-messages";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  _email: string;
+  _password: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthClientService,
+    private flashMessage: FlashMessagesService,
+    private route: Router
+  ) {}
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if (auth) {
+        return this.route.navigate(["/"]);
+      }
+    });
   }
 
+  get email() {
+    return this._email;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  set email(value) {
+    this._email = value;
+  }
+
+  set password(value) {
+    this._password = value;
+  }
+
+  onLogin() {
+    this.authService
+      .login(this.email, this.password)
+      .then(auth => {
+        if (auth) {
+          this.flashMessage.show("You are logged successfully", {
+            cssClass: "alert-success",
+            timeout: 5000
+          });
+
+          this.route.navigate(["/"]);
+        }
+      })
+      .catch(error => {
+        this.flashMessage.show(error.message, {
+          cssClass: "alert-danger",
+          timeout: 10000
+        });
+      });
+  }
 }
--- src/app/services/auth-client.service.ts
+++ src/app/services/auth-client.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthClientService {
+  constructor(private fireauth: AngularFireAuth) {}
 
-  constructor() { }
+  login(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.fireauth.auth.signInWithEmailAndPassword(email, password).then(
+        userData => resolve(userData),
+        error => reject(error)
+      );
+    });
+  }
+
+  getAuth() {
+    return this.fireauth.authState.pipe(map(auth => auth));
+  }
 }
